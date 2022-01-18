3edce86e9562afb57ca8f8045aa305a75f27504e
SeyoungCho1 <45449387+SeyoungCho@users.noreply.github.com>
2022-01-18 09:27:56 +0000

    added comments

--- Greedy/largeNumber.py
+++ Greedy/largeNumber.py
@@ -4,24 +4,25 @@ N,M,K = map(int, input().split())
 data = list(map(int, input().split()))
 
 start_time = time.time()
+
+#sort the list and reverse it
 tmpData = list(sorted(data))
 tmpData.reverse()
-tmpData = [int(x) for x in tmpData]
-print(tmpData)
+
 count = M
 sum = 0
+
 while True:
-  for i in range(K):
-    if(count == 0):
+  for i in range(K):  
+    if(count == 0): #iterate M times
       break
-    sum += tmpData[0]
+    sum += tmpData[0] #add tmpData[0](the largest) K times
     count -= 1
   if(count == 0):
     break
-  sum += tmpData[1]
+  sum += tmpData[1] #add second largest number 
   count -= 1
 
-       
 end_time = time.time()
 print(sum)
 print("time :", end_time - start_time)   
