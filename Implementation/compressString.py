bdb30f4319215966fec5ab3526ae674b3e55418a
SeyoungCho1 <45449387+SeyoungCho@users.noreply.github.com>
2022-02-14 07:16:46 +0000

    solved compressString problem

--- Implementation/compressString.py
+++ Implementation/compressString.py
@@ -3,32 +3,37 @@ start_time = time.time()
 
 def solution(s):
   answer = len(s)
-  for i in range(1, len(s)//2):
-    total_length = 0
-    partial_length = i
+  for i in range(1, len(s)//2 + 1):
     j = 1
+    cur = s[0:j*i]
+    total_len = len(cur)
     cnt = 1
-    cur = s[0:i]
-    while (j+1)*i <= len(s):
-      if cur == s[j*i:(j+1)*i]:
-        cnt += 1
-        j += 1
-        if partial_length == i:
-          partial_length += 1
-          total_length += partial_length
-      else:
+    #continue till the end of the string
+    while True:
+      if (j+1) * i > len(s):
         if cnt > 1:
-          partial_length += 1
-        total_length += partial_length
-        partial_length = i
-        cnt = 1
-        cur = s[j*i:(j+1)*i]
-        j += 1
-    if total_length < answer:
-      answer = total_length
+          total_len += 1
+        break
+      nex = s[j*i:(j+1)*i]
+      #if it matches the next string
+      if cur == nex:
+        cnt += 1
+      #if it does not match
+      else:
+        #add 1 to total length if there are more than one repeated
+        if cnt > 1:
+          total_len += 1
+          #cnt goes back to 1
+          cnt = 1
+        cur = nex
+        total_len += len(cur)
+      j += 1      
+    total_len += (len(s) - j*i)
+    if total_len < answer:
+      answer = total_len
   return answer
 
-result = solution("aabbaa")
+result = solution("aaaaaaaabcbca")
 print(result)
 end_time = time.time() 
 print("time :",end_time - start_time)
